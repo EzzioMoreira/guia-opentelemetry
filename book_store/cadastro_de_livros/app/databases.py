0d45dcd5de361ada481de08a2c83498d6a7a7f26
Ezzio Moreira <ezzio.moreira@gmail.com>
2025-01-14 19:48:28 -0300

    feat: add cadastro de livros

---
+++ book_store/cadastro_de_livros/app/databases.py
@@ -0,0 +1,62 @@
+"""
+Modulo responsável por criar a conexão com o banco de dados
+"""
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import time
+import psycopg2
+import logging
+
+# Configura o nível de log
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+# Obtem as credenciais do banco de dados das variáveis de ambiente
+DB_USER = os.getenv("DB_USER", "user")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
+DB_HOST = os.getenv("DB_HOST", "db")
+DB_NAME = os.getenv("DB_NAME", "bookstore")
+
+# URL de conexão com o banco de dados
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
+
+# Função que aguarda até que o banco de dados esteja disponível
+def wait_for_db():
+    while True:
+        try:
+            # Tentativa de conexão com o banco de dados
+            logger.info("Tentando conexão com o banco de dados...")
+            conn = psycopg2.connect(
+                host=DB_HOST,
+                database=DB_NAME,
+                user=DB_USER,
+                password=DB_PASSWORD
+            )
+            conn.close()
+            logger.info("Conexão com o banco de dados estabelecida!")
+            break
+        except psycopg2.OperationalError as e:
+            logger.info(f"Conexão com o banco de dados falhou: {e}")
+            time.sleep(1)
+
+wait_for_db()
+
+# Cria a engine de conexão com o banco de dados
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# Função que retorna uma sessão do banco de dados
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    except Exception as e:
+        logger.error(f"Erro ao acessar o banco de dados: {e}")
+        db.rollback()
+        raise
+    finally:
+        db.close()
+        logger.info("Conexão com o banco de dados  encerrada")
---
+++ book_store/cadastro_de_livros/app/main.py
@@ -0,0 +1,62 @@
+"""
+Função principal que cria a aplicação FastAPI
+"""
+from fastapi import FastAPI, HTTPException, Depends
+from sqlalchemy.orm import Session
+import uvicorn
+from . import models
+import logging
+from .databases import engine, get_db
+
+# Configura o nível de log
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+# Cria as tabelas no banco de dados
+models.Base.metadata.create_all(bind=engine)
+
+# Cria a aplicação FastAPI
+app = FastAPI()
+
+# Define a rota para criar um livro
+@app.post("/livros/")
+def cria_livro(livro: models.LivroBase, db: Session = Depends(get_db)):
+    try:
+        logger.info(f"Criando livro: {livro}")
+        novo_livro = models.cria_livro(db=db, livro=livro)
+        logger.info(f"Livro criado com sucesso: {livro}")
+        return novo_livro
+    except Exception as e:
+        logger.error(f"Erro ao criar livro: {e}")
+        raise HTTPException(status_code=500, detail="Erro ao criar livro")
+
+# Define a rota para listar livros por id
+@app.get("/livros/{id}")
+def busca_livro(id: int, db: Session = Depends(get_db)):
+    try:
+        logger.info(f"Buscando livro com id: {id}")
+        livro = models.busca_livro(db, id)
+        if livro is None:
+            logger.warning(f"Livro com id {id} não encontrado")
+            raise HTTPException(status_code=404, detail="Livro não encontrado")
+        logger.info(f"Livro com ID: {id} encontrado com sucesso")
+        return livro
+    except Exception as e:
+        logger.error(f"Erro ao buscar livro: {e}")
+        raise HTTPException(status_code=500, detail="Erro ao buscar livro")
+
+# Define a rota para listar todos os livros
+@app.get("/livros/")
+def lista_livros(db: Session = Depends(get_db)):
+    try:
+        logger.info("Listando todos os livros")
+        livros = models.lista_livros(db)
+        logger.info(f"{len(livros)} livros encontrados")
+        return livros
+    except Exception as e:
+        logger.error(f"Erro ao listar livros: {e}")
+        raise HTTPException(status_code=500, detail="Erro ao listar livros")
+
+# Configuração para rodar o servidor 
+if __name__ == "__main__": 
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8080, reload=True)
