612ab51d504f5ee2582760bbe50fde9111361d09
Ezzio Moreira <ezzio.moreira@gmail.com>
2025-01-15 21:48:06 -0300

    feat: add sistema ordem de compra

---
+++ book_store/ordem_de_compra/app/__init__.py
@@ -0,0 +1,8 @@
+"""
+Indica que o diretório é um pacote Python
+"""
+import logging
+
+# Configuração global de logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
---
+++ book_store/ordem_de_compra/app/models.py
@@ -0,0 +1,49 @@
+"""
+Modulo responsável por manipular os dados do banco de dados
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from .databases import Base
+from . import logger
+
+# Defini a classe OrdemBase contendo os campos livro_id para serem usados na criação de uma ordem
+class OrdemBase(BaseModel):
+    livro_id: int
+    
+# Defini a classe OrdemCreate contendo os campos livro_id para serem usados na criação de uma ordem
+class OrdemCreate(OrdemBase):
+    pass
+
+# Defini a classe Ordem incluindo os campos id, status e pagamento_status para serem usados na criação de uma ordem
+class Ordem(OrdemBase):
+    id: int # Identificação da ordem
+    status: str # Status da ordem (ex. Pendente, Aprovado, Cancelado)
+    pagamento_status: str # Status do pagamento (ex. Pendente, Aprovado, Cancelado)
+    
+    class Config:
+        orm_mode = True # Configuração para permitir a leitura de objetos ORM
+
+# Defini a classe OrdemDB contendo os campos para criação da tabela no banco de dados
+class OrdemDB(Base):
+    __tablename__ = "ordens" # Nome da tabela no banco de dados
+    id = Column(Integer, primary_key=True, index=True) # Campo de identificação da ordem
+    livro_id = Column(Integer) # Campo de identificação do livro
+    status = Column(String) # Campo de status da ordem
+    pagamento_status = Column(String) # Campo de status do pagamento
+
+# Função para criar uma ordem
+def cria_ordem(db: Session, ordem: OrdemCreate):
+    db_ordem = OrdemDB(
+        livro_id=ordem.livro_id,
+        status="Pendente",
+        pagamento_status="Pendente"
+    )
+    db.add(db_ordem)
+    db.commit()
+    db.refresh(db_ordem)
+    return db_ordem
+
+# Função para listar uma ordem
+def lista_ordem(db: Session, ordem_id: int):
+    return db.query(OrdemDB).filter(OrdemDB.id == ordem_id).first()
